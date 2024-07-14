20e6f47e46e04c1d211e107feb38057d2af7a7d9
MyNiuuu <muyao.niu@gmail.com>
2024-07-15 01:14:08 +0800

    upload training codes

--- Training/train_stage1.sh
+++ Training/train_stage1.sh
@@ -3,13 +3,13 @@ EXP_NAME="train_stage1"
 accelerate launch train_stage1.py \
  --pretrained_model_name_or_path="./ckpts/stable-video-diffusion-img2vid-xt-1-1" \
  --output_dir="logs/${EXP_NAME}/" \
- --width=128 \
- --height=128 \
+ --width=384 \
+ --height=384 \
  --seed=42 \
  --learning_rate=2e-5 \
  --per_gpu_batch_size=1 \
  --num_train_epochs=5 \
- --mixed_precision="no" \
+ --mixed_precision="fp16" \
  --gradient_accumulation_steps=1 \
  --checkpointing_steps=2500 \
  --checkpoints_total_limit=100 \
